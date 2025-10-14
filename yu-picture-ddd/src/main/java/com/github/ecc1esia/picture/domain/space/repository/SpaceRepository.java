ca054fcbfb2539e74fd340e6ea6a72d992210fe7
jieyuu <ecc1esia@outlook.com>
2025-10-14 17:46:53 +0800

    feat(space): 添加空间和空间用户仓储接口
    - 新增 SpaceRepository 接口，继承 IService<Space>
    - 新增 SpaceUserRepository 接口，继承 IService<SpaceUser>
    - 定义空间相关实体的仓储操作接口
    - 完善 .gitignore 文件，移除重复和不必要的忽略规则

---
+++ yu-picture-ddd/src/main/java/com/github/ecc1esia/picture/domain/space/repository/SpaceRepository.java
@@ -0,0 +1,10 @@
+package com.github.ecc1esia.picture.domain.space.repository;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.ecc1esia.picture.domain.space.entity.Space;
+
+/**
+ * 空间仓储
+ */
+public interface SpaceRepository extends IService<Space> {
+}
---
+++ yu-picture-ddd/src/main/java/com/github/ecc1esia/picture/domain/space/repository/SpaceUserRepository.java
@@ -0,0 +1,10 @@
+package com.github.ecc1esia.picture.domain.space.repository;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.ecc1esia.picture.domain.space.entity.SpaceUser;
+
+/**
+ * 空间用户仓储
+ */
+public interface SpaceUserRepository extends IService<SpaceUser> {
+}
